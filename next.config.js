1d55423eb4b497bff38666b3b29fd0251d5d5fa3
emilMircea <emil-mircea@protonmail.com>
2019-01-15 19:03:34 +0100

    remove sass from config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 const withImages = require("next-images");
 const withCSS = require("@zeit/next-css");
 const withFonts = require("next-fonts");
-const withSass = require("@zeit/next-sass");
-module.exports = withSass(withFonts(withImages(withCSS())));
+// const withSass = require("@zeit/next-sass");
+module.exports = withFonts(withImages(withCSS()));
