651c8681e1c605cf8b015a589cf1ea3540ff8bdd
emilMircea <emil-mircea@protonmail.com>
2019-01-16 22:52:13 +0100

    turn class component into functional component

--- pages/PageLayout.js
+++ pages/PageLayout.js
@@ -1,19 +1,18 @@
-import React, {PureComponent, Component} from 'react'
-import PropTypes from 'prop-types';
-import Sidebar from '../components/sidebar/Sidebar';
-import './layout.css'
+import React, { PureComponent, Component } from "react";
+import PropTypes from "prop-types";
+import Sidebar from "../components/sidebar/Sidebar";
+import Head from "next/head";
+import "./layout.css";
 
-class PageLayout extends Component {
-  
-  render() {
-    return(
-      <div className="grid-container">
-        {this.props.children}
-      </div>
-    )
-  }
-  
-}
+const PageLayout = ({ children }) => (
+  <div className="grid-container">
+    <Head>
+      <title>Carla Lluis Portfolio</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    {children}
+  </div>
+);
 
-
-export default PageLayout
+export default PageLayout;
