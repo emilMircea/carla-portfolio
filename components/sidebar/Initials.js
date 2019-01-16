f65080ec08dcdf727ba225ee391c98c8528e92cd
emilMircea <emil-mircea@protonmail.com>
2019-01-16 22:53:14 +0100

    remove is required from propTypes and add prettier

--- components/sidebar/Initials.js
+++ components/sidebar/Initials.js
@@ -2,25 +2,31 @@ import React from "react";
 import "animate.css/animate.min.css";
 import PropTypes from "prop-types";
 
-const Initials = ({ initials, left }) => (
-  <div
-    className="animated slideInLeft"
-    style={{
-      backgroundImage: `url(../static/${initials}.svg)`,
-      backgroundSize: "contain",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center center",
-      width: "100%",
-      height: "150px",
-      position: "relative",
-      left: `${left ? `-${left}rem` : "-1.6rem"}`,
-      animationDuration: ".5s"
-    }}
-  />
-);
+const Initials = ({ initials, left }) => {
+  if (initials) {
+    return (
+      <div
+        className="animated slideInLeft"
+        style={{
+          backgroundImage: `url(../static/${initials}.svg)`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+          width: "100%",
+          height: "150px",
+          position: "relative",
+          left: `${left ? `-${left}rem` : "-1.6rem"}`,
+          animationDuration: ".5s"
+        }}
+      />
+    );
+  } else {
+    return null;
+  }
+};
 
 Initials.propTypes = {
-  initials: PropTypes.string.isRequired,
+  initials: PropTypes.string,
   left: PropTypes.string
 };
 
