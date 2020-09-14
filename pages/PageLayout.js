d81b3931d76fcf5b27f80ef08975d66027a806e0
emil mircea <emil-mircea@protonmail.com>
2020-09-14 17:44:20 +0200

    rm unused imports

--- pages/PageLayout.js
+++ pages/PageLayout.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React;
 import PropTypes from "prop-types";
 import Sidebar from "../components/sidebar/Sidebar";
 import Head from "next/head";
